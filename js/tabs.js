e3018cfee01993c9ef9ffdc0b7935bc0fe23ae35
Jangarashev <122522399+Jangarashev@users.noreply.github.com>
2023-03-21 13:16:25 +0600

    Add files via upload

---
+++ js/tabs.js
@@ -0,0 +1,39 @@
+const reviewsSwitcher = document.querySelector('#reviewsSwitcher');
+
+// функиця показа нужного отзыва по имени
+const findReview = (reviewId) => {
+  // находим текущий отображаемый отзыв (у него должен быть активный класс)
+  const activeReview = document.querySelector('.reviews__item--active');
+  // снимаем с текущего отображаемого отзыва активный класс
+  activeReview.classList.remove('reviews__item--active');
+  // находим нужный отзыв по имени (по селектору дата атрибута)
+  const currentReview = document.querySelector(`.reviews__list[data-item="${reviewId}"]`);
+  // и ставим на него активный класс (фактически показываем нужный отзыв)
+  currentReview.classList.add('reviews__item--active');
+};
+// при кликах на список аватарок
+reviewsSwitcher.addEventListener("click", (e) => {
+  e.preventDefault();
+  // находим по какому именно элементу кликнули
+  const target = e.target;
+  // если кликнули на картинку
+  if (target.classList.contains('interactive-avatar__img')) {
+    // ищем текущую активную аватарку
+    const activeListItem = document.querySelector('.reviews__switcher-item--active');
+    // если нашли
+    if (activeListItem) {
+      // убираем с неё активный класс (зеленую рамку)
+      activeListItem.classList.remove('reviews__switcher-item--active');
+    }
+    // у картинки есть родитель - кнопка
+    const button = target.parentElement;
+    // а у кнопки есть родитель <li>
+    const listElement = button.parentElement;
+    // берем у кнопки имя нужного для показа отзыва
+    const id = button.getAttribute('data-open');
+    // с <li> снимаем активный класс 
+    listElement.classList.add('reviews__switcher-item--active');
+    // запускаем функцию показа отзыва и передаем полученное с кнопки имя отзыва 
+    findReview(id);
+  }
+});

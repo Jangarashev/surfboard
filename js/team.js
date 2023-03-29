f2d20c056bb67cde099c81725ddafb049eea1120
Jangarashev <122522399+Jangarashev@users.noreply.github.com>
2023-03-29 15:39:18 +0600

    Add files via upload

--- js/team.js
+++ js/team.js
@@ -1,33 +1,30 @@
-const getSizeOfBlock = block => block.height();
-
-const openItem = item => {
-  const textBlock = item.find(".team__content-block");
-  const reqHeight = getSizeOfBlock(textBlock);
-  const textContainer = item.find(".team__content");
-
-  item.addClass("active");
-  textContainer.height(reqHeight);
-};
-
-const closeEveryItem = container => {
-  const items = container.find(".team__item");
-  const textBlocks = container.find(".team__content");
-
-  items.removeClass("active");
-  textBlocks.height(0);
-};
-
-$(".team__item").click(e => {
-  e.preventDefault();
-
-  const $this = $(e.currentTarget);
-  const itemOpened = $this.hasClass("active");
-  const container = $this.closest(".team");
+const openItem = (item) => {
+    const container = item.closest(".team__item");
+    const contentBlock = container.find(".team__content");
+    const textBlock = contentBlock.find(".team__content-block");
+    const reqHeight = textBlock.height();
   
-  if (itemOpened) {
-    closeEveryItem(container);
-  } else {
-    closeEveryItem(container)
-    openItem($this);
-  }
-});
+    container.addClass("active");
+    contentBlock.height(reqHeight);
+  };
+  
+  const closeEveryItem = (container) => {
+    const items = container.find(".team__content");
+    const itemContainer = container.find(".team__item");
+  
+    itemContainer.removeClass("active");
+    items.height(0);
+  };
+  
+  $(".team__title").click((e) => {
+    const $this = $(e.currentTarget);
+    const container = $this.closest(".team");
+    const elemContainer = $this.closest(".team__item");
+  
+    if (elemContainer.hasClass("active")) {
+      closeEveryItem(container);
+    } else {
+      closeEveryItem(container);
+      openItem($this);
+    }
+  });

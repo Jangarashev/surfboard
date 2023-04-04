c7e60f0c1bf8ac64a03743e1509985e14a03ff42
Jangarashev <122522399+Jangarashev@users.noreply.github.com>
2023-04-04 14:27:39 +0600

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,80 @@
-$(".form").submit(e => {
-    e.preventDefault();
+const validateFields = (form, fieldsArray) => {
 
-    $.fancybox.open({
-        src: "#modal",
-        type: "inline"
-    })
+  fieldsArray.forEach((field) => {
+    field.removeClass("input-error");
+    if (field.val().trim() === "") {
+      field.addClass("input-error");
+    }
+  });
+
+  const errorFileds = form.find("input-error");
+
+  return errorFileds.length === 0;
+}
+
+$('.form').submit(e => {
+  e.preventDefault();
+
+  const form = $(e.currentTarget);
+  const name = form.find("[name='name']");
+  const phone = form.find("[name='phone']");
+  const comment = form.find("[name='comment']");
+  const to = form.find("[name='to']");;
+
+  const modal = $("#modal");
+
+  const content = modal.find(".modal-content")
+
+  modal.removeClass("error-modal");
+
+  const isValid = validateFields(form, [name, phone, comment, to]);
+
+  // [name, phone, comment, to].forEach((field) => {
+    // field.removeClass("input-error");
+    // if (field.val().trim() === "") {
+      // field.addClass("input-error");
+    // }
+  // });
+
+  const errorFileds = form.find("input-error");
+
+  if (isValid) {
+    $.ajax({
+      url: "http://webdev-api.loftschool.com/sendmail",
+      method: "post",
+      data: {
+        name: name.val(),
+        phone: phone.val(),
+        comment: comment.val(),
+        to: to.val(),
+      },
+      success: data => {
+        content.text(data.message)
+
+          $.fancybox.open({
+          src: "#modal",
+          type: "inline"
+          });
+      },
+
+      error: data => {
+        const message = data.responseJSON.message;
+        content.text(message);
+        modal.addClass("error-modal");
+
+        $.fancybox.open({
+          src: "#modal",
+          type: "inline"
+          });
+
+      },
+    });
+  }
+
+  
 });
+
+$(".modal-button").click(e => {
+  e.preventDefault();
+  $.fancybox.close();
+})
